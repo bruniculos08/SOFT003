76f4886d1c2acbe7ce747dd40947d5e918733547
Bruno Rafael dos Santos <62629549+bruniculos08@users.noreply.github.com>
2022-04-27 17:09:51 -0300

    update

---
+++ Gráfico - Atividade 08.py
@@ -0,0 +1,20 @@
+from matplotlib import pyplot as plt
+
+# Sprint 1:
+eixoX = [0, 1, 2, 3, 4, 5]
+# eixoY = tarefas a serem feitas - tarefas feitas realmente:
+eixoY = [4.25, 4.25-(1/3), 4.25-(2/3), 4.25-1, 4.25-1.25, 4.25-3.5]
+
+# Y = tarefas a serem feitas - tarefas a serem feitas de acordo com  a previsão (linearmente):
+X = [0, 1, 2, 3, 4, 5]
+Y = [5*0.85, 4*0.85, 3*0.85, 2*0.85, 1*0.85, 0]
+
+
+plt.plot(X, Y)
+plt.plot(eixoX, eixoY)
+plt.show()
+
+# Sprint 2:
+eixoX = [0, 1, 2, 3]
+# eixoY = tarefas a serem feitas - tarefas feitas realmente:
+eixoY = []
