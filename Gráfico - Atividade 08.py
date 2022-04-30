c6c3733aa158179d31e99b64c0b496b53fe6a9cb
Bruno Rafael dos Santos <62629549+bruniculos08@users.noreply.github.com>
2022-04-30 12:27:14 -0300

    update

--- Gráfico - Atividade 08.py
+++ Gráfico - Atividade 08.py
@@ -9,10 +9,6 @@ S1eixoY = [4.25, 4.25-(1/3), 4.25-(2/3), 4.25-1, 4.25-1.25, 4.25-3.5]
 X = [0, 1, 2, 3, 4, 5]
 Y = [5*0.85, 4*0.85, 3*0.85, 2*0.85, 1*0.85, 0]
 
-plt.plot(X, Y)
-plt.plot(S1eixoX, S1eixoY)
-plt.show()
-
 # Sprint 2:
 S2eixoX = [0, 1, 2, 3]
 # eixoY = tarefas a serem feitas - tarefas feitas realmente:
@@ -22,10 +18,6 @@ S2eixoY = [4.75, 4.75-0.5, 4.75-1, 4.75-2.25]
 X = [0, 1, 2, 3, 4, 5]
 Y = [5*0.95, 4*0.95, 3*0.95, 2*0.95, 1*0.95, 0]
 
-plt.plot(X, Y)
-plt.plot(S2eixoX, S2eixoY)
-plt.show()
-
 # Gráfico SPI e CPI:
 BAC = 500600
 #PV = BAC * P%C
@@ -33,7 +25,14 @@ BAC = 500600
 #SPI = EV/PV = EV/(BAC*P%C)
 #CPI = EV/AC = EV/(BAC*A%C)
 
-SPIeixoX = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-SPIeixoY = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-CPIeixoX = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-CPIeixoY = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+SPIeixoX = [1, 2, 3, 4, 5, 6, 7, 8]
+SPIeixoY = [0.8, 0.7, 0.53, 0.75, 0.96, 0.9, 0.84, 0.78]
+CPIeixoX = [1, 2, 3, 4, 5, 6, 7, 8]
+CPIeixoY = [1.07, 1.11, 1.13, 0.95, 0.96, 0.94, 0.96, 0.91]
+
+plt.plot(SPIeixoX, SPIeixoY, "-b", label="SPI")
+plt.scatter(SPIeixoX, SPIeixoY, marker='o');
+plt.plot(CPIeixoX, CPIeixoY, "-r", label="CPI")
+plt.scatter(CPIeixoX, CPIeixoY, marker='o');
+plt.legend(loc="lower right")
+plt.show()
