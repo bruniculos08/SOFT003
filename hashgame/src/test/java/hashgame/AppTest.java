18c671f5e460989e1f2bd5ae25acc2bc8cdf4173
Bruno Rafael dos Santos <62629549+bruniculos08@users.noreply.github.com>
2022-06-21 10:29:24 -0300

    update

---
+++ hashgame/src/test/java/hashgame/AppTest.java
@@ -0,0 +1,19 @@
+package hashgame;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        TicTacToe game = new TicTacToe();
+        assertEquals(2, game.getResult());
+    }
+}
