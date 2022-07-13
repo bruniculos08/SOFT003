82819848f4137168321b8d1c0e41a0bbdac7173d
Bruno Rafael dos Santos <62629549+bruniculos08@users.noreply.github.com>
2022-07-13 11:13:52 -0300

    update

---
+++ Projeto - Fase 03/Código-Fonte/Model/Cliente.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from Usuário import Usuário
+
+class Cliente(Usuário):
+    pass
---
+++ Projeto - Fase 03/Código-Fonte/Model/Usuário.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Usuário:
+    def __init__(self):
+        self.nome = None
+        self.CPF = None
+        self.email = None
+
+    def deletarUsuario(self, ):
+        pass
+
